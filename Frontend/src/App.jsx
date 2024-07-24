d80c174e4439c386410799f81c13f4915c8bc05e
Suwas52 <suwasdanuwar18@gmail.com>
2024-07-24 15:09:12 +0545

    update folder structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,14 @@
 import { useState } from "react";
 import { Button, Container } from "react-bootstrap";
+import LandingPage from "./pages/User/LandingPage";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="container mt-5">
-      <div className="row">
-        <div className="col-md-12">
-          <Button variant="primary">Primary</Button>
-        </div>
-      </div>
-    </div>
+    <>
+      <LandingPage />
+    </>
   );
 }
 
---
+++ Frontend/src/pages/User/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CustomNavbar from "../../components/Navbar";
+
+const LandingPage = () => {
+  return (
+    <>
+      <CustomNavbar />
+    </>
+  );
+};
+
+export default LandingPage;
