593a2db84a6ca13e8094a8d2d1497ab2faf23190
Suwas52 <suwasdanuwar18@gmail.com>
2024-07-25 23:46:38 +0545

    adding complete landing page

--- Frontend/src/components/UserComponent/Blog.jsx
+++ Frontend/src/components/UserComponent/Blog.jsx
@@ -1,54 +1,67 @@
 import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
+import Bus1 from "../../assets/images/Bus2.jpeg";
+import Bus3 from "../../assets/images/Bus3.jpeg";
+import Bus2 from "../../assets/images/Bus6.jpeg";
+import Date from "@mui/icons-material/CalendarMonth";
 
 const Blog = () => {
   return (
-    <section className="blog-section">
+    <section className="blog-section ">
       <Container>
-        <h2>Recent Blog Post</h2>
+        <h2 className="text-center">Recent Blog Post</h2>
+        <div className="description text-center container-fluid">
+          <p>
+            Have a look at our popular reason. why you should choose you bus.
+            Just a bus and get a ticket for your great journey
+          </p>
+        </div>
         <Row>
           <Col md={4}>
             <Card>
-              <Card.Img variant="top" src="bus1.jpg" />
+              <Card.Img variant="top" src={Bus1} />
               <Card.Body>
-                <Card.Title>The standard Lorem Ipsum</Card.Title>
+                <Date className="icon" />
+                <small className="text-muted">19 Feb 2022</small>
+              </Card.Body>
+              <Card.Footer>
+                <h5>The standard Lorem Ipsum</h5>
                 <Card.Text>
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Aenean commodo ligula eget dolor.
                 </Card.Text>
-              </Card.Body>
-              <Card.Footer>
-                <small className="text-muted">19 Feb 2022</small>
               </Card.Footer>
             </Card>
           </Col>
           <Col md={4}>
             <Card>
-              <Card.Img variant="top" src="bus2.jpg" />
+              <Card.Img variant="top" src={Bus2} />
               <Card.Body>
-                <Card.Title>Lorem Ipsum is simply dummy</Card.Title>
-                <Card.Text>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenean commodo ligula eget dolor.
-                </Card.Text>
+                <Date className="icon" />
+                <small className="text-muted">19 Feb 2022</small>
               </Card.Body>
               <Card.Footer>
-                <small className="text-muted">19 Feb 2022</small>
+                <h5>Lorem Ipsum is simply dummy</h5>
+                <Card.Text>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the…
+                </Card.Text>
               </Card.Footer>
             </Card>
           </Col>
           <Col md={4}>
             <Card>
-              <Card.Img variant="top" src="bus3.jpg" />
+              <Card.Img variant="top" src={Bus3} />
               <Card.Body>
-                <Card.Title>Why do we use it?</Card.Title>
-                <Card.Text>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Aenean commodo ligula eget dolor.
-                </Card.Text>
+                <Date className="icon" />
+                <small className="text-muted">19 Feb 2022</small>
               </Card.Body>
               <Card.Footer>
-                <small className="text-muted">19 Feb 2022</small>
+                <h5>Why do we use it?</h5>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Aenean commodo ligula eget dolor.
+                </p>
               </Card.Footer>
             </Card>
           </Col>
--- Frontend/src/components/UserComponent/Footer.jsx
+++ Frontend/src/components/UserComponent/Footer.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import Facebook from "@mui/icons-material/FacebookOutlined";
+import Twitter from "@mui/icons-material/Twitter";
+import Instagram from "@mui/icons-material/Instagram";
+import Linkedin from "@mui/icons-material/LinkedIn";
+import Logo from "../../assets/images/logo.png";
+import Point from "@mui/icons-material/KeyboardDoubleArrowRightOutlined";
 
 const Footer = () => {
   return (
@@ -8,64 +14,74 @@ const Footer = () => {
         <Row>
           <Col md={4}>
             <h5>ViserBus</h5>
+
             <p>
               Delicious culpa laborum debitis saepe. Commodi earum minus ab
               occaecati veniam deserunt est!
             </p>
             <div className="social-links">
               <a href="#">
-                <i className="fa fa-facebook"></i>
+                <Facebook className="icon" />
               </a>
               <a href="#">
-                <i className="fa fa-twitter"></i>
+                <Twitter className="icon" />
               </a>
               <a href="#">
-                <i className="fa fa-instagram"></i>
+                <Instagram className="icon" />
               </a>
               <a href="#">
-                <i className="fa fa-linkedin"></i>
+                <Linkedin className="icon" />
               </a>
             </div>
           </Col>
           <Col md={2}>
             <h5>Useful Links</h5>
+
             <ul>
               <li>
+                <Point className="icon" />
                 <a href="#">About</a>
               </li>
               <li>
+                <Point />
                 <a href="#">FAQs</a>
               </li>
               <li>
+                <Point />
                 <a href="#">Blog</a>
               </li>
               <li>
+                <Point />
                 <a href="#">Contact</a>
               </li>
             </ul>
           </Col>
-          <Col md={2}>
+          <Col md={3}>
             <h5>Policies</h5>
             <ul>
               <li>
+                <Point className="icon" />
                 <a href="#">Privacy Policy</a>
               </li>
               <li>
+                <Point className="icon" />
                 <a href="#">Terms and Conditions</a>
               </li>
               <li>
+                <Point className="icon" />
                 <a href="#">Ticket Policies</a>
               </li>
               <li>
+                <Point className="icon" />
                 <a href="#">Refund Policy</a>
               </li>
             </ul>
           </Col>
-          <Col md={4}>
+          <Col md={3}>
             <h5>Contact Info</h5>
             <p>Bringi Road Suite Dhaka 1209</p>
             <p>+44 6547 89700</p>
-            <p>example@mail.com</p>
+            <p>yatra@gmail.com</p>
           </Col>
         </Row>
       </Container>
