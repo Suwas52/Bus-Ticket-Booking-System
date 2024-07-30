e26c8cec960dcaec41d30e608b68c8b8cc7b6c69
Amritgrg1 <gamrit450@gmail.com>
2024-07-30 11:33:48 +0545

    completed Admin dashboard UI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,40 @@
-import { useState } from "react";
-import { Button, Container } from "react-bootstrap";
-import LandingPage from "./pages/User/LandingPage";
+import React, { useContext } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Admin/home/Home'
+import Login from './pages/Admin/login/Login'
+import List from './pages/Admin/list/List'
+import Single from './pages/Admin/single/Single'
+import New from './pages/Admin/new/New'
+import { productInputs, userInputs } from './Formsource'
+import "./styles/dark.scss"
+import { DarkModeContext } from './pages/Admin/context/DarkModeContext'
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+
+  const {darkMode} = useContext(DarkModeContext);
 
   return (
-    <>
-      <LandingPage />
-    </>
-  );
+    <div className={darkMode ? "app dark" : "app"}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            <Route index element={<Home />} />
+            <Route path='login' element={<Login/>} />
+            <Route path='users'>
+              <Route index element={<List/>} />
+              <Route path=':userId' element={<Single/>} />
+              <Route path='new' element={<New  inputs={userInputs} title="Add New User"/>} />
+            </Route>
+            <Route path='products'>
+              <Route index element={<List/>} />
+              <Route path=':productId' element={<Single/>} />
+              <Route path='new' element={<New  inputs={productInputs} title="Add New Product"/>} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+export default App
