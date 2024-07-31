ef1b09a9b3f9017949a413985c78a2817b38ec96
Amritgrg1 <gamrit450@gmail.com>
2024-07-31 11:04:21 +0545

    created heroblock component

---
+++ Frontend/src/components/UserComponent/HeroBlock.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import "../../styles/main.scss"
+
+const HeroBlock = ({title, img}) => {
+  return (
+    <div className='img-fluid heroBlock'>
+        <img src={img} alt="" />
+        <div className="hero-title">{title}</div>
+    </div>
+  )
+}
+
+export default HeroBlock
