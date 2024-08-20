590fe2940283c847ae000a6f21872ae700ec2df6
Amritgrg1 <gamrit450@gmail.com>
2024-08-20 17:07:27 +0545

    fixed filterpage css style

--- Frontend/src/pages/User/FilterPage.jsx
+++ Frontend/src/pages/User/FilterPage.jsx
@@ -7,50 +7,51 @@ import Time from "@mui/icons-material/AccessTimeOutlined";
 import Route from "@mui/icons-material/AltRouteOutlined";
 import Bus from "@mui/icons-material/DirectionsBusFilledOutlined";
 import FilterCard from "../../components/UserComponent/Card/FilterCard";
+import SwipeUpAltOutlinedIcon from '@mui/icons-material/SwipeUpAltOutlined';
 
 const FilterPage = () => {
   return (
     <>
       <Header />
-      <div className="container-fluid heropart d-flex justify-content-center align-items-end ">
-        <Container className="top-filter ">
+      <div className="container-fluid heropart d-flex justify-content-center align-items-end">
+        <Container className="top-filter">
           <Form>
             <Row>
-              <Col>
+              <Col xs={12} sm={6} md={3}>
                 <Form.Select className="mb-3">
                   <option>Pickup Point</option>
                   <option value="Dhaka">Dhaka</option>
                   <option value="Chittagong">Chittagong</option>
                 </Form.Select>
               </Col>
-              <Col>
+              <Col xs={12} sm={6} md={3}>
                 <Form.Select className="mb-3">
                   <option>Pickup Point</option>
                   <option value="Dhaka">Dhaka</option>
                   <option value="Chittagong">Chittagong</option>
                 </Form.Select>
               </Col>
-              <Col>
+              <Col xs={12} sm={6} md={3}>
                 <Form.Select className="mb-3">
                   <option>Pickup Point</option>
                   <option value="Dhaka">Dhaka</option>
                   <option value="Chittagong">Chittagong</option>
                 </Form.Select>
               </Col>
-              <Col>
+              <Col xs={12} sm={6} md={3}>
                 <button className="btn btn-success w-100">Find Tickets</button>
               </Col>
             </Row>
           </Form>
         </Container>
       </div>
-      <div className="container-fluid mid">
+      <div className="container-fluid mid pb-5">
         <Container>
           <Row>
-            <Col className="col-3 filter ">
+            <Col xs={12} md={4} lg={3} className="filter mb-4 mb-md-0 mt-5">
               <div className="top-filter">
                 <h5>Filter</h5>
-                <button className=" btn-reset">Reset All</button>
+                <button className="btn-reset">Reset All</button>
               </div>
               <hr />
               <div className="select">
@@ -58,18 +59,6 @@ const FilterPage = () => {
                 <CheckBox name="Classic" Icon={Bus} />
                 <CheckBox name="Coach" Icon={Bus} />
                 <CheckBox name="Ac" Icon={Bus} />
-
-                {/* <div class="form-check mb-3">
-                  <input
-                    class="form-check-input"
-                    type="checkbox"
-                    value=""
-                    id="flexCheckChecked"
-                  />
-                  <label class="form-check-label" for="flexCheckChecked">
-                    Checked checkbox
-                  </label>
-                </div> */}
               </div>
               <hr />
               <div className="select">
@@ -79,14 +68,14 @@ const FilterPage = () => {
               </div>
               <hr />
               <div className="select">
-                <h6 className="mb-1">Routes</h6>
+                <h6 className="mb-1">Departure Time</h6>
                 <CheckBox name=" 06:00 am - 03:30 pm" Icon={Time} />
                 <CheckBox name=" 07:00 am - 04:00 pm" Icon={Time} />
                 <CheckBox name=" 08:00 am - 04:30 pm" Icon={Time} />
               </div>
             </Col>
-            <Col className="col-9">
-              <div className="container-fluid">
+            <Col xs={12} md={8} lg={9} className="mt-5">
+              <div className="container-fluid list">
                 <FilterCard />
                 <FilterCard />
               </div>
